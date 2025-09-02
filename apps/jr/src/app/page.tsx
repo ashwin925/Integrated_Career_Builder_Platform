ddd9f52971c21d4a431d53a1f6f4432271b91f61
ashwin925 <ashwinsundar90250@gmail.com>
2025-09-02 15:39:22 +0530

    foundation done

---
+++ apps/jr/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function WelcomePage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">Welcome to Job Recommendation!</h1>
+    </div>
+  );
+}
---
+++ apps/portal/src/app/page.tsx
@@ -0,0 +1,49 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <h1 className="text-4xl font-bold text-center">Welcome to the Campus Platform Portal</h1>
+      </div>
+
+      <div className="mt-12 grid text-center lg:grid-cols-3 lg:gap-8">
+        <Link href="http://localhost:3001" className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+          <h2 className="mb-3 text-2xl font-semibold">
+            SCB{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Navigate to the SCB application.
+          </p>
+        </Link>
+
+        <Link href="http://localhost:3002" className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+          <h2 className="mb-3 text-2xl font-semibold">
+            LMS{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Navigate to the LMS application.
+          </p>
+        </Link>
+
+        <Link href="http://localhost:3003" className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+          <h2 className="mb-3 text-2xl font-semibold">
+            JR{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Navigate to the JR application.
+          </p>
+        </Link>
+      </div>
+    </main>
+  );
+}
